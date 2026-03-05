013d71c7b3d602a1d352c80a9898ab340871e26b
HexState <Waves3322@proton.me>
2026-03-05 08:29:34 -0500

    Update peps5d_overlay.c

--- peps5d_overlay.c
+++ peps5d_overlay.c
@@ -336,7 +336,8 @@ static void tns5d_gate_2site_generic(Tns5dGrid *g,
     double *Vc_re = (double *)calloc((size_t)chi * svddim_B, sizeof(double));
     double *Vc_im = (double *)calloc((size_t)chi * svddim_B, sizeof(double));
 
-    tsvd_truncated_sparse(Th2_re, Th2_im, svddim_A, svddim_B, chi,
+    tsvd_vesica_truncated_sparse(Th2_re, Th2_im, svddim_A, svddim_B,
+                   D, num_EA, num_EB, chi,
                    U_re, U_im, sig, Vc_re, Vc_im);
     free(Th2_re); free(Th2_im);
 
